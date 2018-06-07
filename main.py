f253585e928c94633dda5173a5a0cd5ddcf9c224
ParserTeam <YuniyOrelTeam@gmail.com>
2018-06-07 09:36:23 +0300

    add algorithm to go trough board by order

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class NpuzzleBoard:
 
     def _create_board(self, board_lines):
         for line in board_lines:
-            if not line.startswith("#"):
+            if line != '\n' and not line.startswith("#"):
                 line = line.split("#")[0]
                 if not self._size:
                     self._size = int(line)
@@ -31,7 +31,34 @@ class NpuzzleBoard:
                         return row, col
 
     def is_solvable(self):
-        print(self.get_empty_position())
+        cur_size = self._size - 1
+        cur_rev_size = 0
+        counter = self._size ** 2
+        reverse = False
+        x = 0
+        y = 0
+        count_error = True if counter % 2 == 1 else False
+
+        while True:
+            while (not reverse and x < cur_size) or (reverse and x > cur_rev_size):
+                print(self._puzzle[y][x])
+                counter -= 1
+                x = x - 1 if reverse else x + 1
+            if not counter:
+                break
+            while (not reverse and y < cur_size) or (reverse and y > cur_rev_size):
+                print(self._puzzle[y][x])
+                counter -= 1
+                y = y - 1 if reverse else y + 1
+            if reverse:
+                x += 1
+                y += 1
+                cur_rev_size += 1
+                cur_size -= 1
+            if count_error and counter == 1:
+                print(self._puzzle[y][x])
+                break
+            reverse = not reverse
         return True
 
     def __str__(self):
