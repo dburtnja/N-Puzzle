06db16143f3c1e25c0822ea584e3dc02cf42471f
mirexcool <mirexcool@gmail.com>
2018-06-09 10:39:26 +0300

    Merge branch 'master' of https://github.com/dburtnja/N-Puzzle
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -69,6 +69,39 @@ class NpuzzleBoard:
                 break
             reverse = not reverse
 
+            def get_zero_index(self):
+                cur_size = self._size - 1
+                cur_rev_size = 0
+                counter = self._size ** 2
+                reverse = False
+                x = 0
+                y = 0
+                count_error = counter % 2 == 1
+                index = 1
+                while True:
+                    while (not reverse and x < cur_size) or (reverse and x > cur_rev_size):
+                        yield self._puzzle[y][x]
+                        counter -= 1
+                        x = x - 1 if reverse else x + 1
+                        index += 1
+                        if (self._puzzle[y][x] == 0):
+                            return (index)
+                    if not counter:
+                        break
+                    while (not reverse and y < cur_size) or (reverse and y > cur_rev_size):
+                        yield self._puzzle[y][x]
+                        counter -= 1
+                        y = y - 1 if reverse else y + 1
+                    if reverse:
+                        x += 1
+                        y += 1
+                        cur_rev_size += 1
+                        cur_size -= 1
+                    if count_error and counter == 1:
+                        yield self._puzzle[y][x]
+                        break
+                    reverse = not reverse
+
     # def _iterate(self, reverse, horizontal, x, y, i):
     #     if not reverse:
     #         while x <
@@ -84,18 +117,18 @@ class NpuzzleBoard:
     #         self._iterate()
 
     def is_solvable(self):
-        if self._size % 2 == 0:
-            ordered_puzzle = sum(self._puzzle, [])
-        else:
-            ordered_puzzle = list(self.go_by_order())
+        # if self._size % 2 == 0:
+        # ordered_puzzle = sum(self._puzzle, [])
+        # else:
+        ordered_puzzle = list(self.go_by_order())
         empty_index = ordered_puzzle.index(0)
         print(empty_index)
         my_sum = 0
 
         for i in range(self._size ** 2):
-            # print(ordered_puzzle[i:])
+            print(ordered_puzzle[i:])
             l = len([n for n in ordered_puzzle[i:] if n < ordered_puzzle[i] and n != 0])
-            # print(l)
+            print(l)
             my_sum += l
         if self._size % 2 == 0:
             empty_row_position = self.get_empty_position_row() + 1
@@ -103,7 +136,7 @@ class NpuzzleBoard:
             print("sum: " + str(my_sum))
             return (my_sum + empty_row_position) % 2 == 0
         else:
-            return my_sum % 2 == 0
+             return my_sum % 2 == 0
 
     def __str__(self):
         return str(self._puzzle)
@@ -112,12 +145,14 @@ class NpuzzleBoard:
 if __name__ == "__main__":
     generate = True
     if generate:
-        out = check_output(['python', 'npuzzle-gen.py', '3'])
+        out = check_output(['python', 'npuzzle-gen.py', '4'])
         print(out)
         board = NpuzzleBoard(out)
     else:
         puzzle_file = askopenfile()
         board = NpuzzleBoard(puzzle_file)
     if not board.is_solvable():
-        print("This puzzle is unsolvable")
+        print("# This puzzle is unsolvable")
+    else:
+        print("# This puzzle is solvable")
     print(board)
